fd154226a5784d2ad39d40fcf49d85b4a985dc74
Yotam Bar-On <tudmotu@gmail.com>
2014-06-29 23:45:41 +0300

    Revert "return to using all of jquery-ui due to dep issues"
    This reverts commit 87b76b69581ca33e70905e32defa182abb35b28a.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 requirejs.config({
     baseUrl : '.',
     paths   : {
-        'lib'              : 'vendor',
-        'modules'          : 'js',
-        'template_dir'     : 'html',
-        'angular'          : 'vendor/angular/angular',
-        'ng-resource'      : 'vendor/ng-resource/dist/ng-resource',
-        'ui-sortable'      : 'vendor/angular-ui-sortable/sortable.min',
-        'jquery-ui'        : 'vendor/jquery-ui/jquery-ui.min',
-        'jquery'           : 'vendor/jquery/dist/jquery.min',
-        'jqui-touch-punch' : 'vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.min',
-        'text'             : 'vendor/requirejs-text/text'
+        'lib'               : 'vendor',
+        'modules'           : 'js',
+        'template_dir'      : 'html',
+        'angular'           : 'vendor/angular/angular',
+        'ng-resource'       : 'vendor/ng-resource/dist/ng-resource',
+        'ui-sortable'       : 'vendor/angular-ui-sortable/sortable.min',
+        'jquery-ui-core'    : 'vendor/jquery-ui/ui/minified/jquery.ui.core.min',
+        'jquery-ui-widget'  : 'vendor/jquery-ui/ui/minified/jquery.ui.widget.min',
+        'jquery-ui-mouse'   : 'vendor/jquery-ui/ui/minified/jquery.ui.mouse.min',
+        'jquery-ui-sortable': 'vendor/jquery-ui/ui/minified/jquery.ui.sortable.min',
+        'jquery'            : 'vendor/jquery/dist/jquery.min',
+        'jqui-touch-punch'  : 'vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.min',
+        'text'              : 'vendor/requirejs-text/text'
     },
     shim    : {
         'angular': {
@@ -20,13 +23,13 @@ requirejs.config({
             deps: ['angular']
         },
         'ui-sortable': {
-            deps: ['angular', 'jquery-ui']
+            deps: ['angular', 'jqui-touch-punch', 'jquery-ui-core', 'jquery-ui-widget', 'jquery-ui-mouse', 'jquery-ui-sortable']
         },
         'jquery-ui-core': {
             deps: ['jquery']
         },
         'jqui-touch-punch': {
-            deps: ['jquery-ui']
+            deps: ['jquery-ui-widget', 'jquery-ui-mouse']
         }
     }
 });
@@ -34,7 +37,6 @@ requirejs.config({
 requirejs([
     'modules/Application',
     'vendor/mobile-detect/mobile-detect',
-    'jqui-touch-punch',
     'angular'
 ], function (Appliction) {
     function bootstrapAngular () {
