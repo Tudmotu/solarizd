9b868329057c6296da1563e68911cbf0f063173a
Yotam Bar-On <tudmotu@gmail.com>
2015-08-13 23:43:02 +0300

    Added some features to sol-tabs; Added more tests; Added jasmine matcher libraries

--- src/modules/ui-kit/sol-tabs/sol-tabs.js
+++ src/modules/ui-kit/sol-tabs/sol-tabs.js
@@ -5,6 +5,29 @@ export default [function () {
         restrict: 'E',
         transclude: true,
         replace: true,
-        template: '<div class="sol-tabs"><ng-transclude></ng-transclude></div>'
+        template: '<div class="sol-tabs"><ng-transclude></ng-transclude></div>',
+        scope: {
+            selected: '@'
+        },
+        link: function ($scope, $element, $attrs) {
+            $scope.getSelected = function () {
+                return parseInt($scope.selected, 10) || 0;
+            };
+            $scope.applySelection = function () {
+                if ($scope.tabs.length) {
+                    $scope.tabs.forEach((el) => {
+                        el.removeAttribute('selected');
+                    });
+                    $scope.tabs[$scope.getSelected()].setAttribute('selected', '');
+                }
+            };
+
+            $scope.tabs = $element.find('[tab-id]').toArray();
+
+            $scope.applySelection();
+            $scope.$watch('selected', () => {
+                $scope.applySelection();
+            });
+        }
     };
 }];
--- src/test/ui-kit/sol-tabs.js
+++ src/test/ui-kit/sol-tabs.js
@@ -1,5 +1,7 @@
 import UIKit from '../../modules/ui-kit/ui-kit.js';
+import JasmineMatchers from '../../vendor/jasmine-expect/dist/jasmine-matchers';
 import JasmineJQuery from '../../vendor/jasmine-jquery/lib/jasmine-jquery.js';
+import $ from '../../vendor/jquery/dist/jquery.js';
 import {module, inject} from '../mocks';
 
 describe('solTabs directive', function () {
@@ -12,7 +14,59 @@ describe('solTabs directive', function () {
         $rootScope = _$rootScope_;
     }));
 
-    it('Should transclude its content and wrap it in .sol-tabs', () => {
+    it('changes selected tab when "selected" attribute changes', () => {
+        let html = '<div><sol-tabs selected="{{selected}}">' +
+                        '<div tab-id="one"></div>' +
+                        '<div tab-id="two"></div>' +
+                        '<div tab-id="three"></div>' +
+                    '</sol-tabs></div>';
+        let element = $compile(html)($rootScope);
+        let scope = element.find('.sol-tabs').isolateScope();
+
+        $rootScope.selected = 1;
+        $rootScope.$apply();
+
+        expect(scope.tabs[1]).toHaveAttr('selected');
+        expect(scope.tabs[0]).not.toHaveAttr('selected');
+        //expect(scope.tabs[2]).not.toHaveAttr('selected');
+    });
+
+    it('selects the first tab by default', () => {
+        let html = '<div><sol-tabs>' +
+                        '<div tab-id="one"></div>' +
+                        '<div tab-id="two"></div>' +
+                        '<div tab-id="three"></div>' +
+                    '</sol-tabs></div>';
+        let element = $compile(html)($rootScope);
+        let scope = element.find('.sol-tabs').isolateScope();
+
+        expect(scope.tabs[0]).toHaveAttr('selected');
+    });
+
+    it('should contain three tabs', () => {
+        let html = '<div><sol-tabs>' +
+                        '<div tab-id="one"></div>' +
+                        '<div tab-id="two"></div>' +
+                        '<div tab-id="three"></div>' +
+                    '</sol-tabs></div>';
+        let element = $compile(html)($rootScope);
+        let scope = element.find('.sol-tabs').isolateScope();
+
+        expect(scope.tabs).toBeArrayOfSize(3);
+    });
+
+    it('should contain two tabs', () => {
+        let html = '<div><sol-tabs>' +
+                        '<div tab-id="one"></div>' +
+                        '<div tab-id="two"></div>' +
+                    '</sol-tabs></div>';
+        let element = $compile(html)($rootScope);
+        let scope = element.find('.sol-tabs').isolateScope();
+
+        expect(scope.tabs).toBeArrayOfSize(2);
+    });
+
+    it('should transclude its content and wrap it in .sol-tabs', () => {
         let html = '<div><sol-tabs>content content</sol-tabs></div>';
         let element = $compile(html)($rootScope);
 
