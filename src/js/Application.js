1fb4e8b4bdba81e083a690892890497012bda5ee
Yotam Bar-On <tudmotu@gmail.com>
2015-08-21 02:19:00 +0300

    Refactor solTabs to use Object.assign with $scope

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,3 +1,4 @@
+import 'babel/polyfill';
 import './Footer';
 import './Topbar';
 import './Search';
--- src/modules/ui-kit/sol-tabs/sol-tabs.js
+++ src/modules/ui-kit/sol-tabs/sol-tabs.js
@@ -5,31 +5,32 @@ export default [function () {
         restrict: 'E',
         transclude: true,
         replace: true,
-        //template: '<div class="sol-tabs"><header class="tabs"><span class="tab" ng-repeat="tab in tabs"></span></header><section class="wrapper"><ng-transclude></ng-transclude></section></div>',
         templateUrl: '/modules/ui-kit/sol-tabs/sol-tabs.html',
         scope: {
             selected: '@'
         },
         link: function ($scope, $element, $attrs) {
-            $scope.select = function (selected) {
-                $scope.selected = selected;
-            };
+            Object.assign($scope, {
+                get tabs () {
+                    return $element.find('[tab-id]').toArray().map((o) => {
+                        return {
+                            id: o.getAttribute('tab-id'),
+                            title: o.getAttribute('tab-title'),
+                            icon: o.getAttribute('tab-icon'),
+                            $el: o
+                        };
+                    });
+                },
+                select: (selected) => {
+                    $scope.selected = selected;
+                },
+                getIconClass: (icon) => {
+                    if (!!icon)
+                        return 'fa fa-' + icon;
 
-            $scope.tabs = $element.find('[tab-id]').toArray().map((o) => {
-                return {
-                    id: o.getAttribute('tab-id'),
-                    title: o.getAttribute('tab-title'),
-                    icon: o.getAttribute('tab-icon'),
-                    $el: o
-                };
+                    return '';
+                }
             });
-
-            $scope.getIconClass = function (icon) {
-                if (!!icon)
-                    return 'fa fa-' + icon;
-
-                return '';
-            };
 
             $scope.$watch('selected', (selected) => {
                 selected = parseInt(selected, 10) || 0;
