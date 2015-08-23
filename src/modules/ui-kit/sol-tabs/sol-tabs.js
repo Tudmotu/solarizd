bd200839854cd7090bcdf2e5388f6033de02477d
Yotam Bar-On <tudmotu@gmail.com>
2015-08-24 00:32:06 +0300

    Refactor the selected $watch callback to be defined as a method on $scope

--- src/modules/ui-kit/sol-tabs/sol-tabs.js
+++ src/modules/ui-kit/sol-tabs/sol-tabs.js
@@ -29,21 +29,22 @@ export default [function () {
                         return 'fa fa-' + icon;
 
                     return '';
+                },
+                applySelection: () => {
+                    let selected = parseInt($scope.selected, 10) || 0;
+
+                    if ($scope.tabs.length) {
+                        $scope.tabs.forEach((tab) => {
+                            $element.find(`[tab-id="${tab.id}"]`)[0].removeAttribute('selected');
+                        });
+
+                        let selectedId = $scope.tabs[selected].id;
+                        $element.find(`[tab-id="${selectedId}"]`)[0].setAttribute('selected', '');
+                    }
                 }
             });
 
-            $scope.$watch('selected', (selected) => {
-                selected = parseInt(selected, 10) || 0;
-
-                if ($scope.tabs.length) {
-                    $scope.tabs.forEach((tab) => {
-                        $element.find(`[tab-id="${tab.id}"]`)[0].removeAttribute('selected');
-                    });
-
-                    let selectedId = $scope.tabs[selected].id;
-                    $element.find(`[tab-id="${selectedId}"]`)[0].setAttribute('selected', '');
-                }
-            });
+            $scope.$watch('selected', $scope.applySelection);
         }
     };
 }];
