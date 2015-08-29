9cfa2baf512898cb1a6579abaa3f2556b1cd3697
Yotam Bar-On <tudmotu@gmail.com>
2015-08-29 22:51:56 +0300

    Add .bar element to playlist-item

--- src/test/playlist/item.js
+++ src/test/playlist/item.js
@@ -91,6 +91,13 @@ describe('playlistItem directive', function () {
     });
 
     describe('general markup', () => {
+        it('contains .bar element as a direct child of .playlist-item', () => {
+            let html = '<div><playlist-item title="test bla"></playlist-item></div>';
+            let root = createRoot(html, $rootScope);
+
+            expect(root.find('.playlist-item > .bar')).toHaveLength(1);
+        });
+
         it('contains item title inside the .title element', () => {
             let html = '<div><playlist-item title="test bla"></playlist-item></div>';
             let root = createRoot(html, $rootScope);
