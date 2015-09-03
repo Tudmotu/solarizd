c5e86c48007f58fde1bb1f34dd62aca943e88224
Yotam Bar-On <tudmotu@gmail.com>
2015-09-03 19:46:20 +0300

    playlistItem adds now-playing class when progress > 0

--- src/test/playlist/item.js
+++ src/test/playlist/item.js
@@ -193,6 +193,13 @@ describe('playlistItem directive', function () {
     });
 
     describe('general markup', () => {
+        it('has .now-playing class when progress > 0', () => {
+            let html = '<div><playlist-item progress="1"></playlist-item></div>';
+            let root = createRoot(html, $rootScope);
+
+            expect(root.find('.playlist-item')).toHaveClass('now-playing');
+        });
+
         it('contains .actions-bar.bar element', () => {
             let html = '<div><playlist-item title="test bla"></playlist-item></div>';
             let root = createRoot(html, $rootScope);
