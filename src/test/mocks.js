81f23f3318622cfa57f2e2bd4ba647b45af7b241
Yotam Bar-On <tudmotu@gmail.com>
2015-11-01 01:05:06 +0200

    Add some mock utilities

--- src/test/mocks.js
+++ src/test/mocks.js
@@ -3,3 +3,28 @@ import AngularMocks from '../vendor/angular-mocks/angular-mocks';
 
 export let module = angular.mock.module;
 export let inject = angular.mock.inject;
+
+export function createRootFactory ($compile) {
+    return (html, scope) => {
+        let el = $compile(html)(scope);
+        scope.$digest();
+        return el;
+    };
+}
+
+const NOOP = () => {};
+function mockMethods (data, methods) {
+    methods.forEach((method) => {
+        data[method] = NOOP;
+        spyOn(data, method);
+    });
+
+    return data;
+}
+export let FirebaseMock = {
+    mockArray (data) {
+        return mockMethods(data, [
+            '$remove'
+        ]);
+    }
+};
